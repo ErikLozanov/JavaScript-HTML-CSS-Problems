ac8717c0c396dbdccb9cb327d3e6140852526929
ErikLozanov <wolfierecordss@gmail.com>
2023-05-15 16:28:36 +0300

    3rd problem solved

--- JS DOM Introduction/03. Accordion/accordion.js
+++ JS DOM Introduction/03. Accordion/accordion.js
@@ -1,3 +1,14 @@
 function toggle() {
-    console.log('TODO:...');
+    let moreBtnElement = document.querySelector('.head .button');
+    let moreBtnElementText = moreBtnElement.textContent;
+    if(moreBtnElementText === 'More') {
+        let extraTextElement = document.getElementById('extra');
+        extraTextElement.style.display ='block';
+        moreBtnElement.textContent = 'Less'
+    }
+    if(moreBtnElementText === 'Less') {
+        let extraTextElement = document.getElementById('extra');
+        extraTextElement.style.display ='none';
+        moreBtnElement.textContent = 'More'
+    }
 }
