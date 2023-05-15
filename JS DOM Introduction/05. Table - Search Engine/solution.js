1549b53ecbb67b91379ba35266c594aa8fdd90c7
ErikLozanov <wolfierecordss@gmail.com>
2023-05-15 17:49:08 +0300

    Update solution.js

--- JS DOM Introduction/05. Table - Search Engine/solution.js
+++ JS DOM Introduction/05. Table - Search Engine/solution.js
@@ -2,7 +2,17 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-
+   let studentInfoElements = document.querySelectorAll('tbody tr')
+   let searchFieldElement = document.getElementById('searchField').value;
+   studentInfoElements.forEach(x=> x.classList.remove('select'))
+   if(searchFieldElement.length === 0) {
+      return;
+   }
+   for(let row of studentInfoElements) {
+      let infoChecker = row.textContent.replace(/\s /g,'').trim();
+      if(infoChecker.includes(searchFieldElement)) {
+         row.className = 'select';
+      }
+   }
    }
 }
