f3efa4cafd24f76d8bdba82ef2a8aa0473e4b43d
ErikLozanov <wolfierecordss@gmail.com>
2023-05-15 21:35:22 +0300

    Update solution.js

--- JS DOM Introduction/06. Format the Text/solution.js
+++ JS DOM Introduction/06. Format the Text/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO
+  let textElement = document.getElementById("input").value;
+  let outputElement = document.getElementById("output");
+  let textSplit = textElement.split(".");
+  textSplit.pop();
+  let textSplitCopy = textSplit.slice();
+  // outputElement.innerHTML = '';
+  for(let el of textSplit) {
+    if(el.length < 1) {
+      return;
+    }
+  }
+  for(let el of textSplitCopy) {
+    if(textSplit.length < 3) {
+    let end = textSplit.splice(0,textSplit.length);
+    outputElement.innerHTML += `<p> ${end.join('.')}. </p>` 
+      break;
+    }
+    let splicer = textSplit.splice(0,3);
+    outputElement.innerHTML += `<p> ${splicer.join('.')}. </p>` 
+  }
 }
