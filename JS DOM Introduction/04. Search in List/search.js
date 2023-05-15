aa5f2fcfbf61b96024b507ba69c758b017c9831a
ErikLozanov <wolfierecordss@gmail.com>
2023-05-15 16:47:56 +0300

    Update search.js

--- JS DOM Introduction/04. Search in List/search.js
+++ JS DOM Introduction/04. Search in List/search.js
@@ -1,3 +1,17 @@
 function search() {
-   // TODO
+   let townElements = document.getElementsByTagName('li')
+   let townElementsArr = Array.from(townElements);
+   let searchTextElement = document.getElementById('searchText').value;
+   let matchesCounter = 0;
+   townElementsArr.forEach(x => x.style='text-decoration: none ; font-weight: normal;')
+   for(let town of townElementsArr) {
+
+      if(town.textContent.includes(searchTextElement)) {
+         town.style ='text-decoration: underline ; font-weight: bold;';
+         matchesCounter++;
+      }
+   }
+   let matchResultElement = document.getElementById('result');
+   matchResultElement.textContent = `${matchesCounter} matches found`
 }
+
