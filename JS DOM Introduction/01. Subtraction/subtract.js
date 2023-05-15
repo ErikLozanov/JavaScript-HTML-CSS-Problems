d3426021e7c7f9abdaf51078a65bbb6c52854f1c
ErikLozanov <wolfierecordss@gmail.com>
2023-05-15 13:16:58 +0300

    First Problem Solved

--- JS DOM Introduction/01. Subtraction/subtract.js
+++ JS DOM Introduction/01. Subtraction/subtract.js
@@ -1,3 +1,7 @@
 function subtract() {
-    console.log('TODO:...');
+    let firstNumElement = document.getElementById('firstNumber').value;
+    let secondNumElement = document.getElementById('secondNumber').value;
+    let sum = Number(firstNumElement) - Number(secondNumElement);
+    let resultElement = document.getElementById('result');
+    resultElement.textContent = sum;
 }
