f03f7d5d0083e4671f6d5a6f1bb843fb7075a1fe
ErikLozanov <wolfierecordss@gmail.com>
2023-05-15 22:16:07 +0300

    Update solution.js

--- JS DOM Introduction/06. Format the Text/solution.js
+++ JS DOM Introduction/06. Format the Text/solution.js
@@ -1,22 +1,45 @@
 function solve() {
-  let textElement = document.getElementById("input").value;
+  let textSplit = document
+    .getElementById("input")
+    .value.split(".")
+    .filter((e) => e.length > 0);
   let outputElement = document.getElementById("output");
-  let textSplit = textElement.split(".");
-  textSplit.pop();
-  let textSplitCopy = textSplit.slice();
-  // outputElement.innerHTML = '';
-  for(let el of textSplit) {
-    if(el.length < 1) {
-      return;
+
+  for (let i = 0; i < textSplit.length; i += 3) {
+    let output = [];
+    for (let j = 0; j < 3; j++) {
+      if (textSplit[i + j]) {
+        output.push(textSplit[i + j]);
+      }
+    }
+    let res = output.join(". ") + ".";
+    outputElement.innerHTML += `<p>${res}</p>`;
+  }
+}
+function solve() {
+  let text = document
+    .getElementById("input")
+    .value.split(".")
+    .filter((e) => e);
+  let output = [];
+
+  for (i = 0; i < text.length; i++) {
+    let sentence = text[i];
+    if (sentence.length > 0) {
+      if (i % 3 === 0 && i != text.length - 1) {
+        output.push("<p>" + sentence + ".");
+      } else if (i % 3 === 1 && i != text.length - 1) {
+        output.push(sentence + ".");
+      } else if (i % 3 === 2 && i != text.length - 1) {
+        output.push(sentence + "." + "</p>");
+      } else {
+        if (i % 3 === 0 && i === text.length - 1) {
+          output.push("<p>" + sentence + "." + "</p>");
+        } else {
+          output.push(sentence + "." + "</p>");
+        }
+      }
     }
   }
-  for(let el of textSplitCopy) {
-    if(textSplit.length < 3) {
-    let end = textSplit.splice(0,textSplit.length);
-    outputElement.innerHTML += `<p> ${end.join('.')}. </p>` 
-      break;
-    }
-    let splicer = textSplit.splice(0,3);
-    outputElement.innerHTML += `<p> ${splicer.join('.')}. </p>` 
-  }
+  document.getElementById("output").innerHTML = output.join(" ");
 }
