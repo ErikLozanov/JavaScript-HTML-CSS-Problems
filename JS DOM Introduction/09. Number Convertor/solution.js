0f21e45a5960ec4fefebb7253bdf5748dc6727e5
ErikLozanov <wolfierecordss@gmail.com>
2023-05-17 13:14:49 +0300

    Last problem solved

--- JS DOM Introduction/09. Number Convertor/solution.js
+++ JS DOM Introduction/09. Number Convertor/solution.js
@@ -1,4 +1,26 @@
 function solve() {
+    let firstOptElement = document.createElement('option');
+    let secondOptElement = document.createElement('option');
+    firstOptElement.value = 'binary';
+    secondOptElement.value = 'hexadecimal';
+    firstOptElement.textContent = 'Binary';
+    secondOptElement.textContent = 'Hexadecimal';
 
-    //TODO...
+    let menuDropDown = document.getElementById('selectMenuTo');
+    menuDropDown.appendChild(firstOptElement);
+    menuDropDown.appendChild(secondOptElement);
+
+    let button = document.getElementsByTagName('button')[0];
+
+    button.addEventListener("click", function () {
+        let inputNumber = document.getElementById('input');
+
+        let outputResult = document.getElementById('result');
+
+        if(menuDropDown.value === 'binary') {
+            outputResult.value = Number(inputNumber.value).toString(2).toUpperCase();
+        } else if(menuDropDown.value ==='hexadecimal') {
+            outputResult.value = Number(inputNumber.value).toString(16).toUpperCase();
+        }
+    })
 }
