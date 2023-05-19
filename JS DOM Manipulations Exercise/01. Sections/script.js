b298e84a5341d61badd1b6f134d627aebb01cf12
ErikLozanov <wolfierecordss@gmail.com>
2023-05-19 14:20:27 +0300

    Update script.js

--- JS DOM Manipulations Exercise/01. Sections/script.js
+++ JS DOM Manipulations Exercise/01. Sections/script.js
@@ -1,3 +1,25 @@
 function create(words) {
-   console.log('TODO:...');
+   let resultElement = document.getElementById('content');
+
+   for(let word of words) {
+
+      let createDivElement = document.createElement('div');
+      let createPelement = document.createElement('p');
+      createPelement.textContent = word;
+      createPelement.style.display ='none';
+      createDivElement.appendChild(createPelement);
+      resultElement.appendChild(createDivElement);
+   }
+   
+   resultElement.addEventListener('click', function(e) {
+      if(e.target.id == 'content') {
+         return
+      }
+      
+      if(e.target.nodeName === 'P') {
+         e.target.style.display ='none';
+      } else {
+         e.target.firstChild.style.display = 'block';
+      }
+   })
 }
