b88cc26f902848b1af46e0f9d83079a2e76d3e50
ErikLozanov <wolfierecordss@gmail.com>
2023-05-21 12:15:08 +0300

    Update solution.js

--- JS DOM Manipulations Exercise/08. Sudomu/solution.js
+++ JS DOM Manipulations Exercise/08. Sudomu/solution.js
@@ -1,4 +1,14 @@
 function solve() {
 
-    //TODO...
+    let checkBtn = document.querySelector('td button');
+    let clearBtn = document.querySelector('td button:nth-of-type(2)');
+    let duplicated = false;
+    let inputs = document.querySelectorAll('tr td input');
+    console.log(inputs);
+    checkBtn.addEventListener('click', check);
+
+    function check() {
+
+
+}
 }
