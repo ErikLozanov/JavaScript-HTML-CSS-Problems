4051cb5fe95c83a1f9ee6f57d29ff10397c0673b
ErikLozanov <wolfierecordss@gmail.com>
2023-05-24 15:45:23 +0300

    Solved

--- JS Adv Funcs Lab/7.Cars.js
+++ JS Adv Funcs Lab/7.Cars.js
@@ -1,3 +1,34 @@
-function cars() {
-    
+function cars(input) {
+    let result = [];
+
+
+    for(let el of input) {
+        if(el.includes('inherit')) {
+            let [not,obj,no,obj1] = el.split(' ');
+
+        } else if(el.includes('set')) {
+            let [cmd,obj,property,value] = el.split(' ');
+
+            result.obj[property] = value;
+        } else if(el.includes('print')) {
+            let [cmd,objName] = el.split(' ');
+            console.log(result.objName);
+        }
+        
+        else {
+        let [cmd,name] = el.split(' ');
+
+        name = {};
+        result.push(name);
+        }
+        
+    }
 }
+cars([
+  "create c1",
+  "create c2 inherit c1",
+  "set c1 color red",
+  "set c2 model new",
+  "print c1",
+  "print c2",
+]);
--- JS Adv Funcs Lab/test.js
+++ JS Adv Funcs Lab/test.js
@@ -1,13 +1,23 @@
 let book = {
-    title: 'Hunter',
-    author: 'Spisarevski',
-}
+  title: "Hunter",
+  author: "Spisarevski",
+};
 
 function bookInfo(year) {
-
-    console.log(`${this.title} was written by ${this.author} and was released in ${year}`);
+  console.log(
+    `${this.title} was written by ${this.author} and was released in ${year}`
+  );
 }
 
-let bookHunter = bookInfo.bind(this,'1999');
+let bookHunter = bookInfo.bind(this, "1999");
 
 bookHunter();
+
+function sort(input, order) {
+  return order === "asc"
+    ? input.sort((a, b) => a - b)
+    : input.sort((a, b) => b - a);
+}
+console.log(sort([14, 7, 17, 6, 8], "asc"));
+
+;
