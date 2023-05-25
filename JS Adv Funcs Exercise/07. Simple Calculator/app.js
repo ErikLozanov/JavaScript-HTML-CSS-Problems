f4ff9837aad41265db77a57729e98f3f998a7dee
ErikLozanov <wolfierecordss@gmail.com>
2023-05-25 13:36:50 +0300

    Update app.js

--- JS Adv Funcs Exercise/07. Simple Calculator/app.js
+++ JS Adv Funcs Exercise/07. Simple Calculator/app.js
@@ -1,7 +1,21 @@
 function calculator() {
-    // TODO:
+    console.log(this.document);
+    return {
+        init(selector1,selector2,resultSelector) {
+            
+            
+            
+        },
+        add() {
+
+        },
+        subtract() {
+
+        }
+    }
 }
 
-
+const calculate = calculator ();
+calculate.init ('#num1', '#num2', '#result');
 
 
