23c1be78c2d1d824381626f678f855ad6c8a33ed
ErikLozanov <wolfierecordss@gmail.com>
2023-05-28 17:45:28 +0300

    5th and 6th problem Solved.

--- JS Unit Testing Exercise/06.Dynamic-Validation/app.js
+++ JS Unit Testing Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,17 @@
 function validate() {
-    // TODO
+   let inputElement = document.getElementById('email');
+
+   inputElement.addEventListener('change', validation);
+
+   function validation(e) {
+   let inputText = e.currentTarget.value;
+   let validatePattern = /\w+@[a-zA-Z]+\.[a-zA-Z]+/g;
+
+   if(validatePattern.test(inputText)) {
+    e.currentTarget.classList.remove('error');
+    return;
+   }
+   e.currentTarget.classList.add('error');
+   return;
+}
 }
