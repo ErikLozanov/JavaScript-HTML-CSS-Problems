08f6bb8c72a498f6051edab39316eb3ea3a3537e
ErikLozanov <wolfierecordss@gmail.com>
2023-05-31 00:41:48 +0300

    Update 7.hex.js

--- JS Classes Exercise/7.hex.js
+++ JS Classes Exercise/7.hex.js
@@ -9,7 +9,25 @@ class Hex {
             return `0x${this.value.toString(16).toUpperCase()}`;
         }
         plus(number) {
-            return this.value+number;
+            let result;
+            if(typeof number === 'object') {
+                result = this.value + number.value;
+            } else {
+                result = this.value + number;
+            }
+            return new Hex(result);
+        }
+        minus(number) {
+            let result;
+            if(typeof number === 'object') {
+                result = this.value - number.value;
+            } else {
+                result = this.value - number;
+            }
+            return new Hex(result);
+        }
+        parse(string) {
+            return parseInt(string,16);
         }
 }
 let FF = new Hex(255);
