d83e94722c23acda31847240e36f5be3e0f7dc46
ErikLozanov <wolfierecordss@gmail.com>
2023-06-03 22:40:29 +0300

    Update 2.wineSelection.js

--- JS Test Preparation/2.wineSelection.js
+++ JS Test Preparation/2.wineSelection.js
@@ -39,17 +39,29 @@ class WineSelection {
     cellarRevision(wineType){
         if(!wineType) {
             let result = [];
-            result.push(`You have space for ${this.space} bottles more.`);
-            result.push(`You paid ${this.bill} for the wine.`);
+            result.push(`You have space for ${this.space - this.wines.length} bottles more.`);
+            result.push(`You paid ${this.bill}$ for the wine.`);
             this.wines.sort((a,b) => a.wineName.localeCompare(b.wineName));
             this.wines.forEach(x => {result.push(`${x.wineName} > ${x.wineType} - ${x.paid ? 'Has Paid' : 'Not Paid'}.`)});
             return result.join('\n');
+        } else {
+            let isFound = this.wines.find(x => x.wineType == wineType);
+
+            if(!isFound) {
+                throw new Error(`There is no ${wineType} in the cellar.`)
+            } else {
+                return `${isFound.wineName} > ${isFound.wineType} - ${isFound.paid ? 'Has Paid' : 'Not Paid'}.`;
+            }
         }
     }
 }
 
-const selection = new WineSelection(2)
-console.log(selection.reserveABottle('Bodegas Godelia Mencía', 'Rose', 144)); 
+const selection = new WineSelection(5)
+selection.reserveABottle('Bodegas Godelia Mencía', 'Rose', 144);
+selection.payWineBottle('Bodegas Godelia Mencía', 144);
+selection.reserveABottle('Sauvignon Blanc Marlborough', 'White', 50);
+selection.reserveABottle('Cabernet Sauvignon Napa Valley', 'Red', 120);
 console.log(selection.cellarRevision());
+
 
 
