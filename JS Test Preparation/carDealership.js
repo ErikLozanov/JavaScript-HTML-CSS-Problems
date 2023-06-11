5c31102a7e23e1647fcdd5488456250095fa4de3
ErikLozanov <wolfierecordss@gmail.com>
2023-06-11 15:47:51 +0300

    Update carDealership.js

--- JS Test Preparation/carDealership.js
+++ JS Test Preparation/carDealership.js
@@ -1,3 +1,21 @@
 class CarDealership {
+    constructor(name) {
+        this.name = name;
+        this.availableCars = [];
+        this.soldCars = [];
+        this.totalIncome = 0;
+    }
 
+    addCar(model, horsepower, price, mileage) {
+        if(model == '' || (!Number.isInteger(horsepower) || horsepower < 0) || price < 0 || mileage < 0) {
+            throw new Error('Invalid input!');
+        }
+        this.availableCars.push({model,horsepower,price,mileage});
+        return `New car added: ${model} - ${horsepower} HP - ${mileage.toFixed(2)} km - ${price.toFixed(2)}$`;
+    }
 }
+
+let dealership = new CarDealership('SoftAuto');
+console.log(dealership.addCar('Toyota Corolla', 100, 3500, 190000));
+console.log(dealership.addCar('Mercedes C63', 300, 29000, 187000));
+console.log(dealership.addCar('', 120, 4900, 240000));
