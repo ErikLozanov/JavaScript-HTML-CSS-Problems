b5249a2a79697ab1ba00b830d7c947a506d0467b
ErikLozanov <wolfierecordss@gmail.com>
2023-06-12 13:56:26 +0300

    Dom Problem

---
+++ JS Test Preparation/01. Work Process_Ресурси/app.js
@@ -0,0 +1,12 @@
+function solve() {
+    let fNameEl = document.getElementById('fname');
+    let lNameEl = document.getElementById('lname');
+    let emailEl = document.getElementById('email');
+    let birthEl = document.getElementById('birth');
+    let positionEl = document.getElementById('position');
+    let salaryEl = document.getElementById('salary');
+
+    let hireBtn = document.getElementById('add-worker');
+    hireBtn.type = 'button';
+}
+solve()
