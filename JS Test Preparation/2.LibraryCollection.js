2d087c2da8c2907af76a5d77114b97ca0d111abc
ErikLozanov <wolfierecordss@gmail.com>
2023-06-13 23:42:36 +0300

    Update 2.LibraryCollection.js

--- JS Test Preparation/2.LibraryCollection.js
+++ JS Test Preparation/2.LibraryCollection.js
@@ -62,6 +62,7 @@ library.payBook('Don Quixote');
 library.addBook('In Search of Lost Time', 'Marcel Proust');
 library.addBook('Ulysses', 'James Joyce');
 console.log(library.getStatistics());
+console.log(library.getStatistics());
 
 
 
