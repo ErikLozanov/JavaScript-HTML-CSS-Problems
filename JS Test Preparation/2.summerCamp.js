467f7210252ecb5984b100430e38c4fecdbf5050
ErikLozanov <wolfierecordss@gmail.com>
2023-06-15 20:21:07 +0300

    Update 2.summerCamp.js

--- JS Test Preparation/2.summerCamp.js
+++ JS Test Preparation/2.summerCamp.js
@@ -30,10 +30,54 @@ class SummerCamp{
 
         return `The ${name} removed successfully.`;
     }
+    timeToPlay(typeOfGame,participant1,participant2) {
+        let isFound = this.listOfParticipants.find(x=> x.name == participant1);
+        
+        if(!isFound) {
+            throw new Error(`Invalid entered name/s.`);
+        }
+        if(typeOfGame == 'WaterBalloonFights') {
+            let isFoundTwo = this.listOfParticipants.find(x=> x.name == participant2);
+            if(!isFoundTwo) {
+            throw new Error(`Invalid entered name/s.`);
+            }
+            if(isFound.condition !== isFoundTwo.condition) {
+                throw new Error(`Choose players with equal condition.`);
+            }
+
+            if(isFound.power > isFoundTwo.power) {
+                isFound.wins++;
+                return `The ${isFound.name} is winner in the game ${typeOfGame}.`
+            } else if(isFound.power < isFoundTwo.power){
+                isFoundTwo.wins++;
+                return `The ${isFoundTwo.name} is winner in the game ${typeOfGame}.`
+            } else {
+                return `There is no winner.`;
+            }
+        } else if(typeOfGame == 'Battleship') {
+            isFound.power += 20;
+
+            return `The ${participant1} successfully completed the game ${typeOfGame}.`
+        }
+    }
+    toString() {
+        let result = [];
+        result.push(`${this.organizer} will take ${this.listOfParticipants.length} participants on camping to ${this.location}`);
+        this.listOfParticipants.sort((a,b)=>b.wins - a.wins);
+        this.listOfParticipants.forEach(x=> result.push(`${x.name} - ${x.condition} - ${x.power} - ${x.wins}`))
+        return result.join('\n');
+    }
 }
 
-The Petar Petarson was successfully registered.
-Uncaught Error: The Petar is not registered in the camp.
-The Petar Petarson removed successfully.
+const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgaria");
+console.log(summerCamp.registerParticipant("Petar Petarson", "child", 300));
+console.log(summerCamp.registerParticipant("Sara Dickinson", "child", 200));
+console.log(summerCamp.timeToPlay("Battleship", "Sara Dickinson"));
+console.log(summerCamp.timeToPlay("WaterBalloonFights","Sara Dickinson","Petar Petarson"));
+
+
+console.log(summerCamp.toString());
+
+
 
 
