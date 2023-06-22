f68dfd3d086bbf3bf1b2a21501210d3556b1bc46
ErikLozanov <wolfierecordss@gmail.com>
2023-06-22 19:04:41 +0300

    Update app.js

--- JS Asynchronous Prgrm Lab/03.Gighub-Commits/app.js
+++ JS Asynchronous Prgrm Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,27 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+
+    let username = document.getElementById('username').value;
+    let repo = document.getElementById('repo').value;
+    let commitsUl = document.getElementById('commits');
+    let url = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+    fetch(url)
+    .then(response =>{
+        if(response.ok === false || response.status !== 200) {
+          return  Promise.reject(`${response.status}: Not Found`);
+        } else {
+         return   response.json()
+
+        }
+    } )
+    .then(data => {
+        commitsUl.innerHTML = '';
+
+        data.forEach(x=> {
+            let li  = document.createElement('li');
+            li.innerHTML = `<p>${x.commit.author.name}:${x.commit.message}`;
+            commitsUl.appendChild(li);
+        })
+    })
+    .catch(error => commitsUl.innerHTML = `<p>${error}</p>`)
 }
