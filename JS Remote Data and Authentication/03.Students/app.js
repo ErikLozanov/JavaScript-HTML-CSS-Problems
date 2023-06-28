afda0bb21dacb6b916bcfa7e9ac0cb407eadbd90
ErikLozanov <122800063+ErikLozanov@users.noreply.github.com>
2023-06-29 00:25:25 +0300

    Created GET request to load all students.

--- JS Remote Data and Authentication/03.Students/app.js
+++ JS Remote Data and Authentication/03.Students/app.js
@@ -1,1 +1,38 @@
-console.log('TODO...');
+loadStudents();
+
+
+async function loadStudents() {
+    let url = 'http://localhost:3030/jsonstore/collections/students';
+    let tbody = document.querySelector('#results tbody');
+    let response = await fetch(url);
+
+    let result = await response.json();
+
+    Object.values(result).forEach(x=> {
+        let tr = createRows(x.firstName,x.lastName,x.facultyNumber,x.grade);
+        tbody.appendChild(tr);
+    })
+}
+
+function createRows(firstName,lastName,facultyNum,grade) {
+    let tr = document.createElement('tr');
+    let tdFirstName = document.createElement('td');
+    tdFirstName.textContent = firstName;
+    let tdLastName = document.createElement('td');
+    tdLastName.textContent = lastName;
+    let tdFacultyNum = document.createElement('td');
+    tdFacultyNum.textContent = facultyNum;
+    let tdGrade = document.createElement('td');
+    tdGrade.textContent = grade;
+
+    tr.appendChild(tdFirstName);
+    tr.appendChild(tdLastName);
+    tr.appendChild(tdFacultyNum);
+    tr.appendChild(tdGrade);
+
+    return tr;
+}
+
+let form = document.getElementById('form');
+
+form.addEventListener('submit',addStudents())
