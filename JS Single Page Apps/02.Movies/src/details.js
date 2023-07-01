766e94242ded7b5dde18cd2cbb6b50a0326a83a8
ErikLozanov <122800063+ErikLozanov@users.noreply.github.com>
2023-07-01 15:57:14 +0300

    Update details.js

--- JS Single Page Apps/02.Movies/src/details.js
+++ JS Single Page Apps/02.Movies/src/details.js
@@ -1,27 +1,25 @@
 import { showView, spinner } from "./util.js";
 
-const detailsSection = document.querySelector('#movie-example');
-
+const detailsSection = document.querySelector("#movie-example");
 
 export function detailsPage(id) {
-    showView(detailsSection);
-    displayMovie(id);
+  showView(detailsSection);
+  displayMovie(id);
 }
 
-
 async function displayMovie(id) {
-    detailsSection.replaceChildren(spinner());
+  detailsSection.replaceChildren(spinner());
 
-    const movie = await getMovie(id);
+  const movie = await getMovie(id);
 
-    detailsSection.replaceChildren(createMovieCard(movie));
-
+  detailsSection.replaceChildren(createMovieCard(movie));
 }
 
 function createMovieCard(movie) {
-    const element = document.createElement('div');
-    element.className = 'container';
-    element.innerHTML = `
+
+  const element = document.createElement("div");
+  element.className = "container";
+  element.innerHTML = `
               <div class="row bg-light text-dark">
             <h1>Movie title: ${movie.title}</h1>
 
@@ -31,22 +29,36 @@ function createMovieCard(movie) {
             <div class="col-md-4 text-center">
               <h3 class="my-3">Movie Description</h3>
               <p>${movie.description}</p>
-              <a class="btn btn-danger" href="#">Delete</a>
-              <a class="btn btn-warning" href="#">Edit</a>
-              <a class="btn btn-primary" href="#">Like</a>
-              <span class="enrolled-span">Liked 1</span>
+                ${createControls(movie)}
             </div>
           </div>
         </div>
     `;
 
-    return element;
+  return element;
+}
+
+function createControls(movie) {
+    const user = JSON.parse(localStorage.getItem("user"));
+    const isOwner = user && user._id == movie._ownerId;
+
+    if(isOwner) {
+        return `
+        <a class="btn btn-danger" href="#">Delete</a>
+        <a class="btn btn-warning" href="#">Edit</a> 
+        `;
+    } else {
+        return `  
+        <a class="btn btn-primary" href="#">Like</a>
+        <span class="enrolled-span">Liked 1</span>
+        `;
+    }
 }
 
 async function getMovie(id) {
-    const res = await fetch(`http://localhost:3030/data/movies/${id}`);
+  const res = await fetch(`http://localhost:3030/data/movies/${id}`);
 
-    const movie = await res.json();
+  const movie = await res.json();
 
-    return movie;
+  return movie;
 }
